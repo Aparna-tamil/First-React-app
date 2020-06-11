994fe7bba62b29e7ff9b6aa0d83b2b86f876d4b2
Aparna-tamil <61375365+Aparna-tamil@users.noreply.github.com>
2020-06-11 08:32:57 +0100

    Add files via upload

---
+++ src/Components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>First react app of TodoList v1.0.0. !!!</p>
+        </React.Fragment>
+    )
+}
+
+
+export default About;
