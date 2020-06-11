0c896b63ccbe60985cff68fdcedb2bf0652a2511
Aparna-tamil <61375365+Aparna-tamil@users.noreply.github.com>
2020-06-11 08:28:56 +0100

    Add files via upload

---
+++ build/precache-manifest.1557249d2041c1d335d533aeca34cf5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882f49188480a8ce6110480e7ba8c2b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b87079e1e80a7ccd52f3",
+    "url": "/static/css/main.8e6e1f79.chunk.css"
+  },
+  {
+    "revision": "f4d132834771b64b8d43",
+    "url": "/static/js/2.54493f2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.54493f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b87079e1e80a7ccd52f3",
+    "url": "/static/js/main.ff1fded9.chunk.js"
+  },
+  {
+    "revision": "759e81ee1e3f967c5c66",
+    "url": "/static/js/runtime-main.66c35898.js"
+  }
+]);
